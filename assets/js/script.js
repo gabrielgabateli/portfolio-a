d4e14cd632c0c00f1912ba6257a42afd7c2d03ca
gabrielgabateli <gabateli16@gmail.com>
2024-04-02 22:29:37 -0300

    Finalizando o formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,49 @@
 // Tratamento de eventos
-document.querySelector("#nome").addEventListener("change", calcular)
-document.querySelector("#nome").addEventListener("change", calcular)
+// document.querySelector("#nome").addEventListener("change", calcular)
+// document.querySelector("#nome").addEventListener("change", calcular)
 
-function calcular() {
-    // DOM - Document Object Model
-    let nome = document.querySelector("#nome").value
 
-    document.querySelector("#mensagemusuario").innerText = nome
+// function calcular() {
+//     // DOM - Document Object Model
+//     let nome = document.querySelector("#nome").value
 
-}
+//     document.querySelector("#mensagemusuario").innerText = nome
+
+    
+// }
+
+let nome = ''
+let sexo = ''
+let ator = ''
+let mensagem = ''
+let nivelFa = 0
+
+const botaoEnviar = document.getElementById('botao_enviar')
+botaoEnviar.addEventListener('click', function() {
+    nome = document.getElementById('nome').value
+    sexo = document.getElementsByName('sexo')
+    ator = document.getElementById('ator').value
+    mensagem = document.getElementById('mensagem').value
+    nivelFa = document.getElementById('prazo').value
+
+    // Loop para saber qual foi o sexo escolhido
+    for (const opcao of sexo) {
+        if (opcao.checked) {
+            sexo = opcao.value
+            break
+        }
+    } 
+
+    // Fazendo a mensagem de confirmação aparecer
+    const mensagemUsuario = document.getElementById('mensagemusuario')
+    mensagemUsuario.style.display = 'block' 
+
+    console.log(nome)
+    console.log(sexo)
+    console.log(ator)
+    console.log(mensagem)
+    console.log("Nivel: " + nivelFa)
+    });
+
 
 
